b5eff8758484b75812e5dd61bd8f8240406e494d
iScarcy <scarcella.giuseppe@gmail.com>
2025-04-01 00:44:14 +0200

    today search

--- events_reminder.py
+++ events_reminder.py
@@ -8,17 +8,29 @@ import sys
 
 def reminder(today):
     print(today)
-    url = "http://scarcypi:3002/api/eventi?oggi=" + today
+    url = "http://scarcypi:3002/api/eventi/today/" + today
     print(url)
     data = requests.get(url).json() 
     
 
     # Iterate through the JSON array
     for item in data:
-        msg = "Oggi è il " + item["type"] + " di " + item["description"]
-        print(msg)
-  #  t = TeleP(msg)
+        msg = ""
+        match(item["type"]):
+            case "Compleanno":
+                anni = date(today).year - date(item["data"]).year  
+                msg = "Oggi " + item["description"] + " compie " + str(anni) + " anni" 
+                print(msg)
+                return
+            case "Onomastico":
+                msg = "Oggi è l'onomastico di " + item["description"]  
+                print(msg)
+            case _:
+                msg = "Evento di oggi " + str(item["data"]) + ": " + item["type"] + " di " + item["description"]    
+                print(msg)
+                return
+        t = TeleP(msg)
 
 if __name__ == "__main__":
-    oggi = str(date.today())
+    oggi = "2025-03-19"#str(date.today())
     reminder(today= oggi)
