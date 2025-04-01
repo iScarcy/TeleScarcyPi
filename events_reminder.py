bda34759431c7dd8b46b58ead94a341327517f26
iScarcy <56608800+iScarcy@users.noreply.github.com>
2025-04-01 22:39:18 +0200

    final commit

--- events_reminder.py
+++ events_reminder.py
@@ -1,37 +1,47 @@
 from tele import TeleP
 from datetime import date
 from datetime import datetime
+import logging
 import requests
     
 
-def reminder(today):
+def reminder(today, log):
     
     url = "http://scarcypi:3002/api/eventi/today/" + today
-    print(url)
+    logger.info("Events reminder, url:" + url )
     data = requests.get(url).json() 
-    
+    logger.info(str(len(data)) + " events was found")
     todayDate = datetime.strptime(today, '%Y-%m-%d').date()
     # Iterate through the JSON array
     for item in data:
         msg = ""
         dateEvent = datetime.strptime(item["data"], '%Y-%m-%d').date()
-        print(item["type"])
+        
         match(item["type"]):
             case "Compleanno":
                 anni = todayDate.year - dateEvent.year  
                 msg = "Oggi " + item["description"] + " compie " + str(anni) + " anni" 
-                print(msg)
+                
                 
             case "Onomastico":
                 msg = "Oggi è l'onomastico di " + item["description"]  
-                print(msg)
+                
                
             case _:
                 msg = "Evento di oggi " + str(item["data"]) + ": " + item["type"] + " di " + item["description"]    
-                print(msg)
-              
+                
+        logger.info("msg:" + msg)      
         t = TeleP(msg)
+
+    logger.info("*** Events reminder, END ***")
 
 if __name__ == "__main__":
     oggi = str(date.today())
-    reminder(today= oggi)
+    logging.basicConfig(filename="log/events_reminder_" + oggi +  ".log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.info("*** Events reminder, START ***")
+    logger.info("today:" + oggi)
+    reminder(today= oggi, log= logger)
