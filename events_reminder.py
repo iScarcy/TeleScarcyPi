7102ff55eb70d1b3cee6602a3bbcadc430c3d2a1
iScarcy <scarcella.giuseppe@gmail.com>
2025-11-20 23:38:25 +0100

    parametrizzazione script

--- events_reminder.py
+++ events_reminder.py
@@ -3,45 +3,55 @@ from datetime import date
 from datetime import datetime
 import logging
 import requests
-    
+import json
+import configparser
+import sys
+import argparse
 
-def reminder(today, log):
-    
-    url = "http://scarcypi:3002/api/eventi/today/" + today
-    logger.info("Events reminder, url:" + url )
-    data = requests.get(url).json() 
-    logger.info(str(len(data)) + " events was found")
-    todayDate = datetime.strptime(today, '%Y-%m-%d').date()
+def reminder(event_type, event_date):
+    print(event_date)
+    url = "http://scarcypi:3002/api/eventi/"+ event_type + "/" + event_date
+    print(url)
+    data = requests.get(url).json()
+
+    if not data:
+        msg = "Nessun evento per oggi " + event_date
+        TeleP(msg)
+        return
+
+    event_date_obj = date.fromisoformat(event_date)
+
     # Iterate through the JSON array
     for item in data:
         msg = ""
-        dateEvent = datetime.strptime(item["data"], '%Y-%m-%d').date()
-        
+        item_date_obj = date.fromisoformat(item["data"])
         match(item["type"]):
             case "Compleanno":
-                anni = todayDate.year - dateEvent.year  
-                msg = "Oggi " + item["description"] + " compie " + str(anni) + " anni" 
-                
-                
+                # Calcolo età considerando mese/giorno
+                anni = event_date_obj.year - item_date_obj.year - (
+                    (event_date_obj.month, event_date_obj.day) < (item_date_obj.month, item_date_obj.day)
+                )
+                msg = "Oggi " + item["description"] + " compie " + str(anni) + " anni"
+                print(msg)
+                TeleP(msg)
+                # non return per gestire eventuali altri eventi nella lista
             case "Onomastico":
-                msg = "Oggi è l'onomastico di " + item["description"]  
-                
-               
+                msg = "Oggi è l'onomastico di " + item["description"]
+                print(msg)
+                TeleP(msg)
             case _:
-                msg = "Evento di oggi " + str(item["data"]) + ": " + item["type"] + " di " + item["description"]    
-                
-        logger.info("msg:" + msg)      
-        t = TeleP(msg)
+                msg = "Evento di oggi " + str(item["data"]) + ": " + item["type"] + " di " + item["description"]
+                print(msg)
+                TeleP(msg)
 
     logger.info("*** Events reminder, END ***")
 
 if __name__ == "__main__":
-    oggi = str(date.today())
-    logging.basicConfig(filename="log/events_reminder_" + oggi +  ".log",
-                    format='%(asctime)s %(message)s',
-                    filemode='w')
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    logger.info("*** Events reminder, START ***")
-    logger.info("today:" + oggi)
-    reminder(today= oggi, log= logger)
+    parser = argparse.ArgumentParser(description="Reminder eventi")
+    parser.add_argument("-type", "--type", dest="event_type", default="today",
+                        help="Tipo di ricerca (es: today)")
+    parser.add_argument("-data", "--data", dest="event_date", default=str(date.today()),
+                        help="Data in formato YYYY-MM-DD (es: 2025-11-20)")
+    args = parser.parse_args()
+
+    reminder(event_type=args.event_type, event_date=args.event_date)
